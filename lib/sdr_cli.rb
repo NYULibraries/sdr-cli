1fdcb6405db8956bc2f14b4ba38bfa536b6446da
Michael Cain <mmc469@nyu.edi>
2023-07-20 13:35:04 -0500

    init gem

---
+++ lib/sdr_cli.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "sdr_cli/version"
+
+module SdrCli
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/sdr_cli/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module SdrCli
+  VERSION = "0.1.0"
+end
