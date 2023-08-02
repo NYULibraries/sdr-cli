d8d41a3a69145b2fb43352dff84a57e4652d7b43
Michael Cain <mmc469@nyu.edi>
2023-08-02 13:46:10 -0500

    eager-loading docs to solve non-problem?

--- spec/sdr_cli/indexer_spec.rb
+++ spec/sdr_cli/indexer_spec.rb
@@ -3,7 +3,7 @@ require "spec_helper"
 RSpec.describe SdrCli::Indexer do
   describe "#indexer" do
     let(:indexer) { described_class.new(solr_url: ENV["SOLR_URL"]) }
-    let(:docs) do
+    let!(:docs) do
       Dir["spec/fixtures/ogm/edu.umn/metadata-aardvark/Datasets/05d-03/*.json"].each_with_object([]) do |json_file, memo|
         memo << JSON.parse(File.read(json_file))
       end
@@ -18,10 +18,9 @@ RSpec.describe SdrCli::Indexer do
     end
     it "should update an existing record from OGM Aardvark into the Solr index" do
       doc = docs.first
-      # doc["dct_alternative_sm"] = "Jawns on Jawns"
-      # indexer.index([doc])
-      # expect(gc_indexer).to have_received(:index).with([doc], commit_within: 5000)
-      expect(docs.keys).to eq 'jawn'
+      doc["dct_alternative_sm"] = "Jawns on Jawns"
+      indexer.index([doc])
+      expect(gc_indexer).to have_received(:index).with([doc], commit_within: 5000)
     end
   end
   it "should create a new Solr index from all records"
