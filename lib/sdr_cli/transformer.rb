a8a53b1426619153ed67a8c65d82f7c479c816ac
Michael Cain <mmc469@nyu.edi>
2023-08-03 14:59:38 -0500

    flesh out transformer

--- lib/sdr_cli/transformer.rb
+++ lib/sdr_cli/transformer.rb
@@ -1,4 +1,21 @@
 module SdrCli
   class Transformer
+    def initialize(directory:)
+      @directory = directory
+    end
+
+    def transform_collection
+      docs = Dir["#{@directory}/**/*.json"]
+      docs.map do |doc|
+        json = JSON.parse(File.read(json_file))
+        transformer(json).run
+      end
+    end
+
+    private
+
+    def transformer(json)
+      GeoCombine::Migrators::V1AardvarkMigrator.new(v1_hash: json)
+    end
   end
 end
