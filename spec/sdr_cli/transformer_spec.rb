e9a4d5b95a0c02e4f69356153267ee620892edf7
Michael Cain <mmc469@nyu.edi>
2023-08-04 15:23:06 -0500

    fix transformer spec

--- spec/sdr_cli/transformer_spec.rb
+++ spec/sdr_cli/transformer_spec.rb
@@ -1,12 +1,11 @@
 require "spec_helper"
 
 RSpec.describe SdrCli::Transformer do
-  let(:aardvark_path) { "spec/fixtures/ogm/edu.umn/metadata-aardvark/Datasets/05d-03/0f3c5f91-37dc-4557-9606-9658ae45a4c8.json" }
-  let(:aardvark_doc) { File.read(aardvark_path) }
-  let(:destination_file) { File.join("spec/fixtures/aardvark", JSON.parse(aardvark_doc)["id"]) }
+  let(:aardvark_path) { "spec/fixtures/aardvark/0f3c5f91-37dc-4557-9606-9658ae45a4c8.json" }
+  let(:aardvark_doc) { JSON.parse(File.read(aardvark_path)) }
   let(:geo_transformer) { instance_double(GeoCombine::Migrators::V1AardvarkMigrator, run: aardvark_doc) }
-  let!(:directory) { "spec/fixtures/ogm/edu.umn/metadata-1.0/Web services/05d-07" }
-  let(:transformer) { described_class.new(directory: directory, destination: "spec/fixtures/aardvark") }
+  let!(:directory) { "spec/fixtures/gbl_1" }
+  let(:transformer) { described_class.new(directory: directory, destination: "tmp") }
 
   before { allow(transformer).to receive(:transformer).and_return(geo_transformer) }
 
@@ -17,9 +16,10 @@ RSpec.describe SdrCli::Transformer do
 
   it "should save the transformed data to the destination" do
     transformer.run
-    expect(File.exist?(destination_file)).to be true
+    file =  File.join("tmp", "#{aardvark_doc["id"]}.json")
+    expect(File.exist?(file)).to be true
 
     # clean up file to ensure test idempotency
-    FileUtils.remove_file(destination_file)
+    # FileUtils.remove_file(destination_file)
   end
 end
