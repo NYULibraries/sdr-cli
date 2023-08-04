fdabac263b651cbbc6c76c28deba23c10be3c283
Michael Cain <mmc469@nyu.edi>
2023-08-04 10:01:45 -0500

    add destination for Transformer

--- lib/sdr_cli/transformer.rb
+++ lib/sdr_cli/transformer.rb
@@ -2,19 +2,37 @@ require "geo_combine/migrators/v1_aardvark_migrator"
 
 module SdrCli
   class Transformer
-    def initialize(directory:)
+    attr_reader :directory, :destination
+
+    def initialize(directory:, destination: )
       @directory = directory
+      @destination = destination
     end
 
-    def transform_collection
-      docs = Dir["#{@directory}/**/*.json"]
+    def run
+      docs = Dir["#{directory}/**/*.json"]
+      new_docs = transform_collection(docs)
+      save_to_destination(new_docs)
+    end
+
+    private
+
+    def transform_collection(docs)
       docs.map do |doc|
         json = JSON.parse(File.read(doc))
         transformer(json).run
       end
     end
 
-    private
+    def save_to_destination(docs)
+      docs.each do |doc|
+        json = JSON.parse(doc)
+        File.open(File.join(destination, json['id']), "w+") do |f|
+          f.write(doc)
+        end
+      end
+    end
+
 
     def transformer(json)
       GeoCombine::Migrators::V1AardvarkMigrator.new(v1_hash: json)
--- spec/sdr_cli/transformer_spec.rb
+++ spec/sdr_cli/transformer_spec.rb
@@ -1,14 +1,26 @@
 require "spec_helper"
 
 RSpec.describe SdrCli::Transformer do
-  let(:geo_transformer) { instance_double(GeoCombine::Migrators::V1AardvarkMigrator, run: true) }
-  let!(:path) { "spec/fixtures/ogm/edu.umn/metadata-1.0/Web services/05d-07" }
-  let(:transformer) { described_class.new(directory: path) }
+
+  let(:aardvark_path) { "spec/fixtures/ogm/edu.umn/metadata-aardvark/Datasets/05d-03/0f3c5f91-37dc-4557-9606-9658ae45a4c8.json" }
+  let(:aardvark_doc) { File.read(aardvark_path) }
+  let(:destination_file) { File.join('spec/fixtures/aardvark', JSON.parse(aardvark_doc)['id']) }
+  let(:geo_transformer) { instance_double(GeoCombine::Migrators::V1AardvarkMigrator, run: aardvark_doc) }
+  let!(:directory) { "spec/fixtures/ogm/edu.umn/metadata-1.0/Web services/05d-07" }
+  let(:transformer) { described_class.new(directory: directory, destination: 'spec/fixtures/aardvark') }
 
   before { allow(transformer).to receive(:transformer).and_return(geo_transformer) }
 
   it "should transform the data from GBL 1.0 to OGM Aardvark" do
-    transformer.transform_collection
-    expect(geo_transformer).to have_received(:run).exactly(Dir["#{path}/**/*.json"].length).times
+    transformer.run
+    expect(geo_transformer).to have_received(:run).exactly(Dir["#{directory}/**/*.json"].length).times
+  end
+
+  it "should save the transformed data to the destination" do
+    transformer.run
+    expect(File.exist?(destination_file)).to be true
+
+    # clean up file to ensure test idempotency
+    FileUtils.remove_file(destination_file)
   end
 end
