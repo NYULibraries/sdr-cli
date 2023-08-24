a11948c2f8a660c5e966fa2679e30556925e7959
Michael Cain <mmc469@nyu.edi>
2023-08-24 15:01:40 -0500

    remove old code and add manual key replacement

--- lib/sdr_cli/indexer.rb
+++ lib/sdr_cli/indexer.rb
@@ -6,8 +6,9 @@ module SdrCli
       @solr_url = solr_url
     end
 
-    def index(dir, commit_within: ENV.fetch("SOLR_COMMIT_WITHIN", 5000).to_i)
-      docs = Dir[dir].map { |file| JSON.parse(File.read(file)) }
+    def index(dir_glob, commit_within: ENV.fetch("SOLR_COMMIT_WITHIN", 5000).to_i)
+      docs = Dir[dir_glob].map { |file| JSON.parse(File.read(file)) }
+      fix_solr_geom_key(docs)
       indexer.index(docs, commit_within: commit_within)
     end
 
@@ -19,6 +20,13 @@ module SdrCli
 
     def solr
       @solr ||= GeoCombine::Indexer.solr(url: @solr_url)
+    end
+
+    # THIS IS HOPEFULLY TEMPORARY AS THIS PR IS IN FLIGHT - https://github.com/OpenGeoMetadata/GeoCombine/pull/143
+    def fix_solr_geom_key(docs)
+      docs.each do |doc|
+        doc.transform_keys! { |key| (key == "solr_geom") ? "dct_bbox" : key }
+      end
     end
   end
 end
