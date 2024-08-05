4cc25da9cc874c07bee28db2d48ff5b9d949c4ad
Brian Kelly <polymonic@gmail.com>
2024-08-05 14:37:50 -0500

    Upgrade to GeoCombine 0.9.1

--- lib/sdr_cli/indexer.rb
+++ lib/sdr_cli/indexer.rb
@@ -6,20 +6,16 @@ module SdrCli
       @solr_url = solr_url
     end
 
-    def index(dir_glob, commit_within: ENV.fetch("SOLR_COMMIT_WITHIN", 5000).to_i)
+    def index(dir_glob)
       files = Dir[dir_glob]
       docs = files.map { |file| JSON.parse(File.read(file)) }
-      indexer.index(docs, commit_within: commit_within)
+      indexer.index(docs)
     end
 
     private
 
     def indexer
-      @indexer ||= GeoCombine::Indexer.new(solr: solr)
-    end
-
-    def solr
-      @solr ||= GeoCombine::Indexer.solr(url: @solr_url)
+      @indexer ||= GeoCombine::Indexer.new(solr: RSolr.connect(url: @solr_url))
     end
   end
 end
