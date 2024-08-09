df7b201b0acbce8b665fe4cebdf5cc0375c5b85e
Brian Kelly <polymonic@gmail.com>
2024-08-09 08:23:15 -0500

    Ruby Standard fixes

--- Rakefile
+++ Rakefile
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
-require 'bundler/gem_tasks'
-require 'rspec/core/rake_task'
+require "bundler/gem_tasks"
+require "rspec/core/rake_task"
 
 RSpec::Core::RakeTask.new(:spec)
 
-require 'standard/rake'
+require "standard/rake"
 
 task default: %i[spec standard]
 
 namespace :dev do
   namespace :auditor do
-    desc 'Test Auditor in development'
+    desc "Test Auditor in development"
     task :light do
-      require_relative 'lib/sdr_cli'
+      require_relative "lib/sdr_cli"
 
       SdrCli::Cli.start(%w[audit --directory ../edu.nyu --destination .])
     end
 
-    desc 'Test Auditor in development'
+    desc "Test Auditor in development"
     task :full do
-      require_relative 'lib/sdr_cli'
+      require_relative "lib/sdr_cli"
 
       SdrCli::Cli.start(%w[audit --directory ../edu.nyu --destination . --check-downloads])
     end
--- lib/sdr_cli/auditor.rb
+++ lib/sdr_cli/auditor.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-require 'json'
-require 'csv'
-require 'nokogiri'
-require 'net/http'
+require "json"
+require "csv"
+require "nokogiri"
+require "net/http"
 
 module SdrCli
   class Auditor
@@ -16,86 +16,86 @@ module SdrCli
     end
 
     def run
-      raise 'Cannot find metadata project directory' unless Dir.exist?(@directory)
-      raise 'Cannot find destination directory' unless Dir.exist?(@destination)
+      raise "Cannot find metadata project directory" unless Dir.exist?(@directory)
+      raise "Cannot find destination directory" unless Dir.exist?(@destination)
 
       csv_path = "#{@destination}/aardvark.csv"
 
-      public_wfs_layers = wfs_layer_names('https://maps-public.geo.nyu.edu/geoserver/sdr/wfs').map do |name|
-        name.split(':')[1]
+      public_wfs_layers = wfs_layer_names("https://maps-public.geo.nyu.edu/geoserver/sdr/wfs").map do |name|
+        name.split(":")[1]
       end.sort!
 
-      restricted_wfs_layers = wfs_layer_names('https://maps-restricted.geo.nyu.edu/geoserver/sdr/wfs').map do |name|
-        name.split(':')[1]
+      restricted_wfs_layers = wfs_layer_names("https://maps-restricted.geo.nyu.edu/geoserver/sdr/wfs").map do |name|
+        name.split(":")[1]
       end.sort!
 
-      public_wms_layers = wms_layer_names('https://maps-public.geo.nyu.edu/geoserver/sdr/wms').sort!
-      restricted_wms_layers = wms_layer_names('https://maps-restricted.geo.nyu.edu/geoserver/sdr/wms').sort!
+      public_wms_layers = wms_layer_names("https://maps-public.geo.nyu.edu/geoserver/sdr/wms").sort!
+      restricted_wms_layers = wms_layer_names("https://maps-restricted.geo.nyu.edu/geoserver/sdr/wms").sort!
 
       headers = %i[category id access format download_url download_status found_in_wfs found_in_wms title]
 
       json_files = Dir.glob("#{@directory}/metadata-aardvark/*/*.json")
 
-      CSV.open(csv_path, 'w', headers:, write_headers: true) do |csv|
+      CSV.open(csv_path, "w", headers:, write_headers: true) do |csv|
         json_files.each_with_index do |path, index|
-          parts = path.split('/')
+          parts = path.split("/")
           category = parts[-2]
           file = parts[-1]
 
-          id = file.split('.').first.gsub('-', '_')
+          id = file.split(".").first.tr("-", "_")
           data = JSON.parse(File.read(path))
-          references = JSON.parse(data['dct_references_s'])
+          references = JSON.parse(data["dct_references_s"])
 
-          wfs_url = references['http://www.opengis.net/def/serviceType/ogc/wfs']
-          wms_url = references['http://www.opengis.net/def/serviceType/ogc/wms']
+          wfs_url = references["http://www.opengis.net/def/serviceType/ogc/wfs"]
+          wms_url = references["http://www.opengis.net/def/serviceType/ogc/wms"]
 
           wfs_existence = if wfs_url
-                            wfs_url.include?('public') ? public_wfs_layers.include?(id) : restricted_wfs_layers.include?(id)
-                          else
-                            'N/A'
-                          end
+            wfs_url.include?("public") ? public_wfs_layers.include?(id) : restricted_wfs_layers.include?(id)
+          else
+            "N/A"
+          end
 
           wms_existence = if wms_url
-                            wms_url.include?('public') ? public_wms_layers.include?(id) : restricted_wms_layers.include?(id)
-                          else
-                            'N/A'
-                          end
+            wms_url.include?("public") ? public_wms_layers.include?(id) : restricted_wms_layers.include?(id)
+          else
+            "N/A"
+          end
 
-          download_url = references['http://schema.org/downloadUrl']
+          download_url = references["http://schema.org/downloadUrl"]
 
           if download_url
             if @check_downloads
               puts "Checking #{index + 1} of #{json_files.size} - #{download_url}..."
               uri = URI(download_url)
               http = Net::HTTP.new(uri.host, uri.port)
-              http.use_ssl = uri.scheme == 'https'
+              http.use_ssl = uri.scheme == "https"
               http.verify_mode = OpenSSL::SSL::VERIFY_NONE
               request = Net::HTTP::Head.new(uri)
 
               begin
-                download_status = http.request(request).is_a?(Net::HTTPSuccess) ? 'Success' : 'Failed'
-              rescue StandardError => e
+                download_status = http.request(request).is_a?(Net::HTTPSuccess) ? "Success" : "Failed"
+              rescue => e
                 download_status = e.message
               end
 
               puts download_status
             else
-              download_status = 'Skipped'
+              download_status = "Skipped"
             end
           else
-            download_status = 'N/A'
+            download_status = "N/A"
           end
 
           csv << [
             category,
             id,
-            data['dct_accessRights_s'],
-            data['dct_format_s'],
+            data["dct_accessRights_s"],
+            data["dct_format_s"],
             download_url,
             download_status,
             wfs_existence,
             wms_existence,
-            data['dct_title_s']
+            data["dct_title_s"]
           ]
         end
       end
@@ -104,13 +104,13 @@ module SdrCli
     def wfs_layer_names(url)
       doc = Nokogiri::XML(Net::HTTP.get(URI("#{url}?service=wfs&version=1.3.0&request=GetCapabilities")))
       doc.remove_namespaces!
-      doc.xpath('//FeatureType/Name').map(&:text)
+      doc.xpath("//FeatureType/Name").map(&:text)
     end
 
     def wms_layer_names(url)
       doc = Nokogiri::XML(Net::HTTP.get(URI("#{url}?service=wms&version=1.3.0&request=GetCapabilities")))
       doc.remove_namespaces!
-      doc.xpath('//Layer/Name').map(&:text)
+      doc.xpath("//Layer/Name").map(&:text)
     end
   end
 end
